142852eb9bb790088d6866ae26062a8cab3654c5
Kartik <kartik15nov@gmail.com>
2020-11-26 09:28:10 +0530

    Added upc in BeerOrderLine. closes #2

--- src/main/java/com/baya/beerorderservice/domain/BeerOrderLine.java
+++ src/main/java/com/baya/beerorderservice/domain/BeerOrderLine.java
@@ -20,14 +20,16 @@ public class BeerOrderLine extends BaseEntity {
     private BeerOrder beerOrder;
 
     private UUID beerId;
+    private String upc;
     private Integer orderQuantity = 0;
     private Integer quantityAllocated = 0;
 
     @Builder(builderMethodName = "beerOrderLineBuilder")
-    public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, BeerOrder beerOrder, UUID beerId, Integer orderQuantity, Integer quantityAllocated) {
+    public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, BeerOrder beerOrder, UUID beerId, String upc, Integer orderQuantity, Integer quantityAllocated) {
         super(id, version, createdDate, lastModifiedDate);
         this.beerOrder = beerOrder;
         this.beerId = beerId;
+        this.upc = upc;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
     }
