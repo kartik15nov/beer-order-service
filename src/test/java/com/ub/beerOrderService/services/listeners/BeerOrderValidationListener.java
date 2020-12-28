5515ac9a30164f7db667c08917c161444b218679
Kartik <kartik15nov@gmail.com>
2020-12-28 21:01:55 +0530

    Added test for Fail validation. closes #20

--- src/test/java/com/ub/beerOrderService/services/listeners/BeerOrderValidationListener.java
+++ src/test/java/com/ub/beerOrderService/services/listeners/BeerOrderValidationListener.java
@@ -10,6 +10,8 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -20,13 +22,16 @@ class BeerOrderValidationListener {
     @JmsListener(destination = JMSConfig.VALIDATE_ORDER_QUEUE)
     public void list(Message<ValidateOrderRequest> message) {
 
+        boolean isValid = true;
+
         ValidateOrderRequest orderRequest = message.getPayload();
 
-        System.out.println("######################## I RAN ########################");
+        if (orderRequest.getBeerOrderDto().getCustomerRef() != null && Objects.equals(orderRequest.getBeerOrderDto().getCustomerRef(), "fail-validation"))
+            isValid = false;
 
         jmsTemplate.convertAndSend(JMSConfig.VALIDATE_ORDER_RESPONSE_QUEUE,
                 ValidateOrderResult.builder()
-                        .isValid(true)
+                        .isValid(isValid)
                         .orderId(orderRequest.getBeerOrderDto().getId())
                         .build());
     }
